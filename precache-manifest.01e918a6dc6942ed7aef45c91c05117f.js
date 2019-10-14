c9d4e0c24c5fa3b8b54a06962fe9955e9f018763
王亚红 <2976716563@qq.com>
2019-10-14 15:42:51 +0800

    first homework

---
+++ precache-manifest.01e918a6dc6942ed7aef45c91c05117f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899cabae8fc1117582b8d0534832d07e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ce5e22558598d288b8",
+    "url": "/static/css/main.a2b16def.chunk.css"
+  },
+  {
+    "revision": "a23a0568b52c3e08dd3a",
+    "url": "/static/js/2.e6e063d1.chunk.js"
+  },
+  {
+    "revision": "b1ce5e22558598d288b8",
+    "url": "/static/js/main.e78fd6dc.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
